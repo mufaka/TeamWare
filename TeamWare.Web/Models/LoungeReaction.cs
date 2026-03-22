9a4cdbf0b5d15346a06f40733205ea321b1b8676
mufaka <bill.nickel@gmail.com>
2026-03-21 18:27:25 -0700

    Add LoungeReaction entity and unit tests
    Refs #85

---
+++ TeamWare.Web/Models/LoungeReaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamWare.Web.Models;
+
+public class LoungeReaction
+{
+    public int Id { get; set; }
+
+    public int LoungeMessageId { get; set; }
+
+    public LoungeMessage LoungeMessage { get; set; } = null!;
+
+    [Required]
+    public string UserId { get; set; } = string.Empty;
+
+    public ApplicationUser User { get; set; } = null!;
+
+    [Required]
+    [StringLength(50)]
+    public string ReactionType { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
